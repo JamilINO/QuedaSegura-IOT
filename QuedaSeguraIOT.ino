9999744b1d94f0282973db977c8a2d9c1ad0eff5
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2024-10-23 18:41:00 -0300

    env

--- QuedaSeguraIOT.ino
+++ QuedaSeguraIOT.ino
@@ -1,3 +1,31 @@
 /*
+  Copyright ©️ 2024 João Vitor de Souza Rodello, Marcela Bedine, Mateus Felipe da Silveira Vieira, Thiago Brito Oliveira dos Santos
 
+      This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/
 */
+
+#include <WiFi.h>
+#include <WiFiClient.h>
+
+
+
+void setup(){
+  Serial.begin(115200);
+  Serial.println("-- Bem Vindo ao Queda Segura --");
+}
+
+void loop(){
+
+  
+}
